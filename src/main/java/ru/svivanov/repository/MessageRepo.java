82fdbb82fc9e3553a1378f5524640d7ec86946d6
Crimscon <62012433+Crimscon@users.noreply.github.com>
2020-08-10 19:04:30 +0300

    added many fixes

--- src/main/java/ru/svivanov/repository/MessageRepo.java
+++
@@ -1,6 +0,0 @@
-package ru.svivanov.repository;
-import org.springframework.data.repository.CrudRepository;
-import ru.svivanov.models.Message;
-
-public interface MessageRepo extends CrudRepository<Message, Long> {
-}
